6adb5d9139dd64e80bfd8ae43cafb9dcc5a1b368
VP <veikko.pursiainen@aalto.fi>
2025-10-04 11:30:46 +0300

    Setting move and zoom actions

--- client_r/src/App.js
+++ client_r/src/App.js
@@ -5,21 +5,32 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 import './App.css';
 
-const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
+//importing the mapbox token from .env
+const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 console.log(MAPBOX_TOKEN);
 
+
+//setting the intiial view
+const INITIAL_CENTER = [ 25.631302 , 64.984528]
+const INITIAL_ZOOM = 4.5
+
+//creating the map
 function App() {
 
   const mapRef = useRef()
   const mapContainerRef = useRef()
 
+  const [center, setCenter] = useState(INITIAL_CENTER)
+  const [zoom, setZoom] = useState(INITIAL_ZOOM)
+
+
     useEffect(() => {
     mapboxgl.accessToken = MAPBOX_TOKEN 
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
-      center: [ 25.631302 , 64.984528],
-      zoom: 4.5
+      center: center,
+      zoom: zoom
     });
 
     return () => {
